96de807e65dad6ed2c0695f56a10192e9cc766b2
RustamNabiullin <nabiullin-90@yandex.ru>
2017-09-23 22:29:42 +0300

    change homework1

--- HomeWork/HomeWork1.java
+++ HomeWork/HomeWork1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Bankomat {
+class HomeWork1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int number = scanner.nextInt();
